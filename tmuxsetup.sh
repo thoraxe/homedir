db4c98083b1047cc4f0bde324f0ff7b20543e206
Erik M Jacobs <erikmjacobs@gmail.com>
2016-01-28 12:30:11 -0500

    would help if the script was right

--- tmuxsetup.sh
+++ tmuxsetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum -y install tmux git
 git init
-git remote add origin https://github.com/homedir.git
+git remote add origin https://github.com/thoraxe/homedir
 git pull origin master
 
